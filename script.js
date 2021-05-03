aae7231dafe6d7c9da9bd3871df4cb704b627c82
sardor4 <83544074+sardor4@users.noreply.github.com>
2021-05-03 22:15:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = prompt('Введите своё имя:');
+var surename = prompt('Введите свою фамилию:');
+var age = +prompt('Введите свой возраст:');
+alert('Зайдите в консоль (CTRL + SIFT + I/F12)');
+console.log('Я ' + name + ' ' + surename + ' мне ' + age + ' лет.');
